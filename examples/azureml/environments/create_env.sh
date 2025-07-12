0451aed95967ace17fa6115977323d8eb12392c3
François Ponchon <francois.ponchon@michelin.com>
2025-07-12 16:05:20 +0200

    Fix training smolvla

--- examples/azureml/environments/create_env.sh
+++ examples/azureml/environments/create_env.sh
@@ -2,4 +2,4 @@ az ml environment create \
     --subscription 270ae3d6-5d7d-47b8-b82d-17f1eea30f72 \
     --resource-group AICPDCNEGBL0 \
     --workspace-name aicpdevcnemlwweu \
-    --file examples/azureml/environments/lerobot_cu126/env.yaml
+    --file examples/azureml/environments/lerobot_cu128/env.yaml
